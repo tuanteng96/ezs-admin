a0a635d4bb0fdb9e12f33a42a8c5acb7e2ca97ca
tuanteng96 <tuantengyn@gmail.com>
2023-04-03 16:35:07 +0700

    update

--- src/app/pages/Clients/pages/Views/index.js
+++ src/app/pages/Clients/pages/Views/index.js
@@ -157,8 +157,74 @@ function ClientViews(props) {
           </div>
         </div>
         <div className="flex-1 p-5 flex flex-col h-full overflow-auto">
-          <div className="mb-5 grow bg-white rounded shadow-sm last:mb-0">
-            <div className="px-5 py-4 font-inter uppercase font-bold text-lg border-b border-separator">
+          <div className="h-full border border-[#d5d7da] rounded-lg flex items-center justify-center flex-col">
+            <svg
+              className="w-16 h-auto"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 64 65"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M32.667 9.833a8 8 0 0 0-8 8h16a8 8 0 0 0-8-8Zm8.944 0a12 12 0 0 0-17.889 0h-9.055a4 4 0 0 0-4 4v42a4 4 0 0 0 4 4h36a4 4 0 0 0 4-4v-42a4 4 0 0 0-4-4H41.61Zm2.37 4c.45 1.273.686 2.625.686 4v2a2 2 0 0 1-2 2h-20a2 2 0 0 1-2-2v-2c0-1.375.236-2.727.686-4h-6.686v42h36v-42H43.98Z"
+                fill="url(#a)"
+              />
+              <path
+                d="M45.114 29.12a2 2 0 0 1-.067 2.827l-14.675 14a2 2 0 0 1-2.762-.001l-7.325-7a2 2 0 1 1 2.764-2.892l5.944 5.68 13.293-12.681a2 2 0 0 1 2.828.066Z"
+                fill="url(#b)"
+              />
+              <defs>
+                <linearGradient
+                  id="a"
+                  x1="17.234"
+                  y1="13.893"
+                  x2="54.531"
+                  y2="44.283"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#7C6BFF" />
+                  <stop offset={1} stopColor="#473DAD" />
+                </linearGradient>
+                <linearGradient
+                  id="b"
+                  x1="17.234"
+                  y1="13.893"
+                  x2="54.531"
+                  y2="44.283"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#7C6BFF" />
+                  <stop offset={1} stopColor="#473DAD" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <div className="mt-3 mb-5 font-bold text-2xl">
+              Không có dịch vụ nào được thiết lập .
+            </div>
+            <div className="max-w-lg mx-auto text-center font-semibold text-[15px] leading-6">
+              Vào
+              <span className="text-primaryhv px-1 font-bold underline cursor-pointer">
+                Quản lý thẻ dịch vụ
+              </span>
+              để sử dụng dịch vụ cho khách hàng.
+              <br />
+              Khách hàng chưa có hoặc mua mới vui lòng chọn
+              <span className="text-primaryhv px-1.5 font-bold underline cursor-pointer">
+                Đăng ký mới
+              </span>
+              để đăng ký buổi lẻ, thẻ liệu trình, thẻ tiền ....
+            </div>
+            <div className="max-w-lg mx-auto text-center font-semibold text-[15px] m-3">
+              Để thanh toán nợ cho khách hàng vui lòng chọn
+              <span className="text-primaryhv px-1.5 font-bold underline cursor-pointer">
+                Thanh toán nợ
+              </span>
+              để xử lý.
+            </div>
+          </div>
+          {/* <div className="mb-5 grow bg-white rounded shadow-sm last:mb-0">
+            <div className="px-5 py-3.5 font-inter uppercase font-bold border-b border-separator">
               Dịch vụ đang thực hiện
             </div>
             <div className="flex p-5 text-[13px] text-muted2 uppercase font-inter font-semibold">
@@ -205,7 +271,7 @@ function ClientViews(props) {
             </div>
           </div>
           <div className="mb-5 bg-white rounded shadow-sm last:mb-0">
-            <div className="px-5 py-4 font-inter uppercase font-bold text-lg border-b border-separator">
+            <div className="px-5 py-3.5 font-inter uppercase font-bold border-b border-separator">
               Thanh toán nợ
             </div>
             <div className="flex p-5 text-muted2 font-semibold uppercase text-[13px] font-inter">
@@ -242,7 +308,7 @@ function ClientViews(props) {
                   </div>
                 ))}
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="w-[350px] pt-5">
