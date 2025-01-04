46bb0ab6d89ff5ba68cb6bd85d35767c1bd3a7f3
tuanteng96 <tuantengyn@gmail.com>
2025-01-04 15:21:16 +0700

    update

--- src/_ezs/partials/ckeditor/CkEditor5.js
+++ src/_ezs/partials/ckeditor/CkEditor5.js
@@ -11,7 +11,7 @@ import {
 
 import UploadAdapter from './UploadAdapter'
 import clsx from 'clsx'
-  console.log(Editor)
+
 function CkEditor5({
   value,
   onChange,
@@ -70,7 +70,58 @@ function CkEditor5({
         <CKEditor
           editor={Editor}
           config={{
-            placeholder: placeholder
+            placeholder: placeholder,
+            heading: {
+              options: [
+                {
+                  model: 'paragraph',
+                  title: 'Paragraph',
+                  class: 'ck-heading_paragraph'
+                },
+                {
+                  model: 'heading1',
+                  view: 'h1',
+                  title: 'Heading 1',
+                  class: 'ck-heading_heading1'
+                },
+                {
+                  model: 'heading2',
+                  view: 'h2',
+                  title: 'Heading 2',
+                  class: 'ck-heading_heading2'
+                },
+                {
+                  model: 'heading3',
+                  view: 'h3',
+                  title: 'Heading 3',
+                  class: 'ck-heading_heading3'
+                },
+                {
+                  model: 'heading4',
+                  view: 'h4',
+                  title: 'Heading 4',
+                  class: 'ck-heading_heading4'
+                },
+                {
+                  model: 'heading5',
+                  view: {
+                    name: 'h3',
+                    classes: 'quick-item quick-level'
+                  },
+                  title: 'Title Quick',
+                  class: 'ck-heading_heading2_fancy'
+                },
+                {
+                  model: 'heading6',
+                  view: {
+                    name: 'h5',
+                    classes: 'quick-item quick-level-sub'
+                  },
+                  title: 'Sub Quick',
+                  class: 'ck-heading_heading2_fancy_sub'
+                }
+              ]
+            }
           }}
           onReady={editor => {
             if (editor && editor.plugins) {
